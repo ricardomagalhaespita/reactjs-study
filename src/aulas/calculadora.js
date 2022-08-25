ffbd5705a39e8e915e6e0fc4c78e3fbdf1cf5746
ricardomagalhaespita <ricardomagalhaespita@gmail.com>
2022-08-25 13:05:36 -0300

    First Commit

---
+++ src/aulas/calculadora.js
@@ -0,0 +1,65 @@
+import {useState} from 'react';
+
+function App() {
+  const [primeiroValor,setPrimeiro] = useState('');
+  const [segundoValor,setSegundo] = useState('');
+  const [resultado,setResultado] = useState('');
+
+  function Soma(){
+    setResultado(parseInt(primeiroValor) + parseInt(segundoValor))
+  }
+  function Subtracao(){
+    setResultado(parseInt(primeiroValor) - parseInt(segundoValor))
+  }
+  function Multiplicacao(){
+    setResultado(parseInt(primeiroValor) * parseInt(segundoValor))
+  }
+  function Divisao(){
+    setResultado(parseInt(primeiroValor) / parseInt(segundoValor))
+  }
+
+  return (
+    <div>
+      <h2>Calculadora!</h2>
+  
+      <form>
+      <b><label>Digite o Primeiro Valor:</label></b>
+        <br/>
+        <input
+        placeholder=""
+        value={primeiroValor}
+        onChange={(e) => setPrimeiro(e.target.value)}
+        />
+
+        <br/>
+        <b><label>Digite o Segundo Valor:</label></b>
+        <br/>
+        <input
+        placeholder=""
+        value={segundoValor}
+        onChange={(e) => setSegundo(e.target.value)}
+        />
+
+        <br/>
+        <b><label>Resultado:</label></b>
+        <br/>
+        <input
+        placeholder=""
+        value={resultado}
+        onChange={(e) => setSegundo(primeiroValor + segundoValor)}
+        />
+
+      </form>
+      <br/>
+
+      <button onClick={Soma}>Soma</button>
+      <button onClick={Subtracao}>Subtração</button>
+      <button onClick={Multiplicacao}>Multiplicação</button>
+      <button onClick={Divisao}>Divisão</button>
+     
+    </div>
+  );
+}
+
+export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 //import './index.css';
-import App from './aulas/aula-03';
+import App from './aulas/calculadora';
+// import App from './aulas/aula-03';
+
 //import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
