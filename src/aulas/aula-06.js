3e8d0e8236eb73f0616c8150c310682f64e221bc
ricardomagalhaespita <ricardomagalhaespita@gmail.com>
2022-09-21 19:59:53 -0300

    First Commit

--- src/aulas/aula-06.js
+++ src/aulas/aula-06.js
@@ -2,18 +2,18 @@ import React, { useState, useEffect } from 'react';
 
 function App(){
 
-const [moveis, setMovies] = useState([]);
+const [distritos, setDitrito] = useState([]);
 
 useEffect (()=>{
     // console.log("useEffect");
 
-    let url = "https://reactnative.dev/movies.json";
+    let url = "https://servicodados.ibge.gov.br/api/v1/localidades/distritos";
 
     fetch(url)
     .then((response) => response.json())
     .then((json) => {
-        console.log(json.movies);
-        setMovies(json.movies);
+        console.log(json);
+        setDitrito(json);
     })
 
 },[]);
@@ -21,14 +21,19 @@ useEffect (()=>{
 
   return(
     <div>
-      <h1>Consultado API</h1>
-
+      <h1>Consultado API de Distritos Brasileiros</h1>
 
       <ul>
+        {distritos.map( distrito => (
+          <li key={distrito.id}>{distrito.nome} - {distrito.municipio.microrregiao.mesorregiao.UF.sigla}</li>
+        ))}
+      </ul>
+
+      {/* <ul>
         {moveis.map( movie => (
           <li key={movie.id}>{movie.title} - {movie.releaseYear}</li>
         ))}
-      </ul>
+      </ul> */}
 
     </div>
   );
